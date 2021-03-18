fbe165851ced5b9b40349ac3e08115415203d0fe
Giorgosoikon <80916783+Giorgosoikon@users.noreply.github.com>
2021-03-18 20:13:02 +0200

    Update roullete.py

--- roullete.py
+++ roullete.py
@@ -1,7 +1,6 @@
 import random
 
 runs = int(input("how many times you want to roll? 1,2 or 3?"))
-
            
 def colour():
     if number < 18:
@@ -9,20 +8,17 @@ def colour():
     else:
         return("big")
 
-
 def oddeven():
     if number % 2 == 0:
         return("even")
     else:
         return("odd")
-    
-
+ 
 def redblack():
     if number<=10 or 19<=number<=28 and number % 2 != 0:
         return("red")
     else:
         return("black")
-
 
 while runs != 1 and runs !=2 and runs !=3 :
     runs = int(input("wrong input try again.how many times you want to roll? 1,2 or 3?"))
